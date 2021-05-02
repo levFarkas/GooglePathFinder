a782f52380bc0cbaab8aef2918ec03c661b747ec
ScalaN22 <>
2021-05-02 14:21:13 +0200

    Add map display and tile downloader

---
+++ src/frontend/image_loader.py
@@ -0,0 +1,49 @@
+import math
+import requests
+from PIL import Image
+import numpy as np
+from typing import List, Type
+from itertools import product
+
+
+def tile_idx_generator(dimensions: int):
+    base = [range(dimensions)] * 2
+    permutations = list(product(*base))
+
+    def generator():
+        for i in range(len(permutations)):
+            yield permutations[i][0], permutations[i][1]
+
+    return generator
+
+
+# source: https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+def deg2num(lat_deg: float, lon_deg: float, zoom: int):
+    lat_rad = math.radians(lat_deg)
+    n = 2.0 ** zoom
+    xtile = int((lon_deg + 180.0) / 360.0 * n)
+    ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
+    return (xtile, ytile)
+
+
+def load_tiles(lat: float, long: float, zoom: int, num_tiles: int) -> List[int]:
+    tile_x, tile_y = deg2num(lat, long, zoom)
+
+    image_data = np.full((256 * num_tiles, 256 * num_tiles, 4), 255)
+    idx_generator = tile_idx_generator(num_tiles)
+    for x_idx, y_idx in idx_generator():
+        image_url = (
+            f"https://tile.openstreetmap.org/{zoom}/{tile_x+x_idx}/{tile_y+y_idx}.png"
+        )
+
+        r = requests.get(image_url, stream=True)
+        if r.status_code == 200:
+            data = Image.open(r.raw)
+            np_image = np.asarray(data.convert("RGBA"))
+            image_data[
+                256 * y_idx : 256 * y_idx + 256, 256 * x_idx : 256 * x_idx + 256, :
+            ] = np_image
+
+        else:
+            break
+    return np.ndarray.flatten(image_data).tolist()
--- src/frontend/pathfinder_gui.py
+++ src/frontend/pathfinder_gui.py
@@ -1,5 +1,9 @@
 import dearpygui.core as core
 import dearpygui.simple as simple
+import threading
+from multiprocessing import Pool
+
+from GooglePathFinder.src.frontend.image_loader import load_tiles
 
 
 class PathFinderGui:
@@ -13,10 +17,68 @@ class PathFinderGui:
         core.set_main_window_size(width=self.width, height=self.height)
         core.set_main_window_resizable(resizable=False)
 
-        with simple.window("Main Window"):
-            core.add_group("main")
+        with simple.window("main_window"):
+            core.add_group("main_group", parent="main_window", horizontal=True)
+            map_display = MapDisplay("map_display", "main_window", 4)
+            map_display.construct()
             core.end()
 
     @staticmethod
     def run():
-        core.start_dearpygui(primary_window="Main Window")
+        core.start_dearpygui(primary_window="main_window")
+
+
+class MapDisplay:
+    def __init__(self, name, parent, num_tiles):
+        self.name = name
+        self.parent = parent
+        self.num_tiles = num_tiles
+        self.pool = Pool()
+
+    def construct(self):
+        with simple.group(self.name, parent=self.parent):
+            core.add_drawing("canvas", width=700, height=700)
+            core.add_texture(
+                "geomap",
+                [255 for i in range(700 * 700 * 4)],
+                256 * self.num_tiles,
+                256 * self.num_tiles,
+            )
+
+            self.pool.apply_async(
+                load_tiles,
+                kwds={
+                    "lat": 46.98828541972894,
+                    "long": 17.933220574578318,
+                    "zoom": 16,
+                    "num_tiles": self.num_tiles,
+                },
+                callback=self.update,
+            )
+
+    def update(self, image_data):
+        core.add_texture(
+            "geomap",
+            image_data,
+            256 * self.num_tiles,
+            256 * self.num_tiles,
+        )
+        core.draw_image("canvas", "geomap", [0, 0], [700, 700])
+
+
+class InputPanel:
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+
+    def construct(self):
+        pass
+
+
+class ExecutionPanel:
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+
+    def construct(self):
+        pass
