0bf35b7feb5c8e3a2404414df366e3d16dbb3ffd
qellin <orsi.sipos.470@gmail.com>
2021-05-09 14:30:21 +0200

    Ez az új tábla szerkezet ez azt jeleni hogy mégis meg kell változtatni a DB connektorban alekérdezést

---
+++ import.sql
@@ -0,0 +1,7 @@
+INSERT INTO PATHFINDER.NODES VALUES
+(1, 'Vörösparti sor - Camping utca', 'Paloznak', 8229, 46.977554, 17.956347,  )
+(2, 'Vörösparti sor 53', 'Paloznak', 8229, 46.976885, 17.954399, )
+;
+
+
+INSERT INTO PATHFINDER.EDGES VALUES(250, 'Vörösparti sor', 1, 2, 170.48)
