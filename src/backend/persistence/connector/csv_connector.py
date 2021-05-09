468db997269281c9f28cb662527d5ea721a1e33d
ScalaN22 <>
2021-05-09 17:30:25 +0200

    Improve performance Data from the CSV is stored as a member

--- src/backend/persistence/connector/csv_connector.py
+++ src/backend/persistence/connector/csv_connector.py
@@ -1,4 +1,5 @@
 import csv
+import logging
 from typing import List, Optional
 
 from GooglePathFinder.src.backend.persistence.connector.interface.connector import Connector
@@ -8,6 +9,7 @@ from GooglePathFinder.src.backend.persistence.connector.model.nodedao import Nod
 class CSVConnector(Connector):
     def __init__(self):
         self.loaded_data = []
+        self.loaded_edges = []
 
     def find_all(self) -> List[NodeDao]:
         if len(self.loaded_data):
@@ -16,23 +18,28 @@ class CSVConnector(Connector):
             self.loaded_data = self._read_nodes(f)
             return self.loaded_data
 
+    def find_all_edges(self):
+        if len(self.loaded_edges):
+            return self.loaded_edges
+        with open("../resources/offline_dataset/edges.csv") as f:
+            self.loaded_edges = self._read_edges(f)
+            return self.loaded_edges
+
     def find_node_by_id(self, node : str) -> Optional[NodeDao]:
         for n in self.find_all():
             if n.node_id == node: return n
         return None
 
     def find_neighbors_by_node(self, node: str) -> List[NodeDao]:
-        with open("../resources/offline_dataset/edges.csv") as f:
-            edges = self._read_edges(f)
-            neighbor_entries = [(edge["TO_ID"], edge["DISTANCE"]) for edge in edges if edge["FROM_ID"] == node]
-            return self._create_neighbor_list(neighbor_entries)
+        edges = self.find_all_edges()
+        neighbor_entries = [(edge["TO_ID"], edge["DISTANCE"]) for edge in edges if edge["FROM_ID"] == node]
+        return self._create_neighbor_list(neighbor_entries)
             
 
     def find_backward_neighbors_by_node(self, node: str) -> List[NodeDao]:
-        with open("../resources/offline_dataset/edges.csv") as f:
-            edges = self._read_edges(f)
-            neighbor_entries = [(edge["FROM_ID"], edge["DISTANCE"]) for edge in edges if edge["TO_ID"] == node]
-            return self._create_neighbor_list(neighbor_entries)
+        edges = self.find_all_edges()
+        neighbor_entries = [(edge["FROM_ID"], edge["DISTANCE"]) for edge in edges if edge["TO_ID"] == node]
+        return self._create_neighbor_list(neighbor_entries)
 
     
     def _create_neighbor_list(self,neighbor_entries: list):
